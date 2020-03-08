ad5ecd9284bdf29fff674e34a6925287188ccb7d
ameliaritger <aritger@ucsb.edu>
2020-03-07 21:37:02 -0800

    Tab 1 plot complete

--- app.R
+++ app.R
@@ -6,31 +6,17 @@ library(shinythemes)
 library(shinyWidgets)
 library(sf)
 library(tmap)
+library(vegan)
+library(gt)
 library(leaflet)
 
-#read in data
+####################################################################
+## Read in data
 reef <- read_csv("MBONReef_Histogram.csv")
 
-#Tidy up data
+## Tidy up the data
 reef_tidy <- reef %>%
-  clean_names() %>%
-  pivot_longer("annelida_cirriformia_luxuriosa":"substrate_amphipod_tube_complex") %>%
-  separate(name, into="phylum", sep="_", remove=FALSE) %>%
-  mutate(vectorized_name=str_split(name, pattern="_")) %>%
-  filter(!phylum=="no") %>% 
-  filter(!phylum=="substrate")
-
-reef_tidy$binary <- ifelse(reef_tidy$value>0, 1, 0)
-reef_tidy$species <- gsub("^[^_]*_","",reef_tidy$name, perl=TRUE)
-
-reef_tidy <- reef_tidy %>%
-  filter(binary > "0")
-
-
-#####
-reef_dontfuckthisup <- reef %>%
   clean_names() %>% #standardize names
-  #rename(latitude=lat_start, longitude=long_start) %>% #rename latitude and longitude
   group_by(location) %>% #group by location to get average lat/long values for each location
   mutate(latitude=mean(lat_start), longitude=mean(long_start)) %>% #get average lat/long values (because that's how that works...)
   ungroup() %>% #really important, we don't want to confuse R!
@@ -41,27 +27,91 @@ reef_dontfuckthisup <- reef %>%
   filter(!phylum=="substrate") #remove substrate values
 
 #Because it's faster to do it outside tidyverse
-reef_dontfuckthisup$binary <- ifelse(reef_dontfuckthisup$value>0, 1, 0) #Add presence/absence column
-reef_dontfuckthisup$species <- gsub("^[^_]*_","",reef_dontfuckthisup$name, perl=TRUE) #Add column for species name
-reef_dontfuckthisup$longitude <- ifelse(reef_dontfuckthisup$longitude<0, reef_dontfuckthisup$longitude, -reef_dontfuckthisup$longitude) #because all longitude values in this region should be negative 
+reef_tidy$binary <- ifelse(reef_tidy$value>0, 1, 0) #Add presence/absence column
+reef_tidy$species <- gsub("^[^_]*_","",reef_tidy$name, perl=TRUE) #Add column for species name
+reef_tidy$longitude <- ifelse(reef_tidy$longitude<0, reef_tidy$longitude, -reef_tidy$longitude) #because all longitude values in this region should be negative 
 
-#Extract only species present
-reef_dontfuckthisup <- reef_dontfuckthisup %>%
+#Do some more tidying
+reef_tidy <- reef_tidy %>%
+  st_as_sf(coords=c("longitude", "latitude"), crs=4326) %>% #Make sticky geometries for lat/long
+  mutate(species = str_replace(species, pattern="tube worm", "tubeworm")) %>%  #replace "tube worm" with "tubeworm" for later grouping
+  separate(species, into="genus", sep="_", remove=FALSE) %>% #Add column for genus name
+  mutate(species=str_replace_all(species, "_", " "),
+         species=str_to_sentence(species)) %>%  #Make species names actually look like species names
+  filter(!str_detect(species, pattern="dead")) #Remove instances where organism is dead
+
+#Now time for some massive if, else statements to group organisms not identified to species
+reef_tidy <- reef_tidy %>% 
+  mutate(grouped_species = ifelse(str_detect(species, pattern = " worm"), "Other worms", ifelse(str_detect(species, pattern = "phoronid"), "Other worms", ifelse(str_detect(species, pattern = "tubeworm"), "Tubeworms", ifelse(str_detect(species, pattern = "algae"), "Other Algaes", ifelse(str_detect(species, pattern = "Filamentous"), "Other Algaes", ifelse(str_detect(species, pattern = "turf"), "Other Algaes", ifelse(str_detect(species, pattern = "blade"), "Other Algaes", ifelse(str_detect(species, pattern = "tunicate"), "Other Tunicates", ifelse(str_detect(species, pattern = "anemone"), "Other anemones", ifelse(str_detect(species, pattern = "bryozoan"), "Other bryozoans", ifelse(str_detect(species, pattern = "White fan"), "Other bryozoans", ifelse(str_detect(species, pattern = "sponge"), "Other Sponges", ifelse(str_detect(species, pattern = "Orange encrusting"), "Other Sponges", ifelse(str_detect(species, pattern = "Haliclona sp"), "Other Sponges", ifelse(str_detect(species, pattern = "zigzag"), "Other Hydroids", species)))))))))))))))) %>%
+  mutate(grouped_genus = ifelse(str_detect(grouped_species, pattern = "Other"), grouped_species, ifelse(str_detect(grouped_species, pattern = "orange"), grouped_species, ifelse(str_detect(grouped_species, pattern = "White"), grouped_species, ifelse(str_detect(grouped_species, pattern = "encrusting"), grouped_species, ifelse(str_detect(grouped_species, pattern = "zigzag"), grouped_species, ifelse(str_detect(grouped_species, pattern = "solitary"), grouped_species, ifelse(str_detect(grouped_species, pattern = "sectioned"), grouped_species, genus)))))))) %>% #do the same for genus
+  mutate(grouped_genus = str_to_title(grouped_genus)) #capitalize first word of genus
+
+####################################################################
+## Subset for a phylum
+reef_phylum <- reef_tidy %>%
   filter(binary > "0") %>% #filter out species not present
-  st_as_sf(coords=c("longitude", "latitude"), crs=4326) #create sticky geometry for lat/long
+  mutate(focal_phylum="annelida") %>% #pick a focal phylum (BASED ON INPUT)
+  mutate(to_match = ifelse(phylum==focal_phylum, filename, "FALSE")) %>% #create a column that we can subset all rows in a plot based on the presence of focal phylum in the plot at least once
+  filter(filename %in% to_match) %>% #if focal phylum is present, keep all observations of that plot ("filename")
+  #filter(!phylum=="annelida") %>% #remove the focal phylum, because we don't need to see co-occurrence of focal phylum with focal phylum (BASED ON INPUT)
+  filter(phylum=="annelida") %>% #select only the coocurring phyla you want to look at (BASED ON INPUT)
+  distinct(filename, .keep_all=TRUE) #remove duplicate values within the same plot
 
+## Subset for a location
+reef_location <- reef_tidy %>%
+  filter(binary > "0") %>% #filter out species not present
+  mutate(focal_location="Rodes") %>% #pick a focal location (BASED ON INPUT)
+  mutate(to_match = ifelse(location==focal_location, filename, "FALSE")) %>% #create a column that we can subset all rows in a plot based on the presence of focal phylum in the plot at least once
+  filter(filename %in% to_match) %>% #if focal phylum is present, keep all observations of that plot ("filename")
+  filter(phylum=="annelida") %>%  #select only the coocurring phyla you want to look at (BASED ON INPUT)
+  distinct(filename, .keep_all=TRUE) #remove duplicate values within the same plot
 
+#Find average abundance values for each location
+reef_summary <- reef_tidy %>%
+  filter(binary > "0") %>% #filter out species not present
+  group_by(location,phylum) %>% #group by location, then lat/long
+  summarize(mean_count = mean(value), #get the mean count
+            median_count = median(value),
+            sd_count = sd(value), #get the s.d. count
+            iqr = IQR(value), #get the interquartile range for the count
+            sample_size = n())
 
+#Create separate dataframe of just latitude, longitude, and locations (use for later plotting species diversity/richness at each location)
+reef_location_unique <- reef_tidy %>% 
+  group_by(location) %>%
+  summarize() #eliminate everything except location and sticky geometries
 
+## Find species diversity/richness for each site
+#Prep data
+reef_vegan <- reef_tidy %>% #named so because of the vegan package!
+  group_by(location,grouped_species) %>% #group by location, then lat/long
+  summarize(mean_count = mean(value)) %>%  #get the mean count
+  select(location, grouped_species, mean_count) %>% 
+  ungroup()
+
+#Calculate species diversity and richness for each site
+reef_vegan_subset <- reef_vegan %>%
+  st_drop_geometry() %>% #remove sticky geometry so it doesn't attach to species name
+  pivot_wider(names_from=grouped_species, values_from=mean_count) %>% 
+  select(`Abietinaria spp`:`Zonaria farlowii`)
+
+diversity <- diversity(reef_vegan_subset)
+richness <- specnumber(reef_vegan_subset)
+
+#Add this information to the vegan dataframe
+reef_vegan <- reef_location_unique %>% 
+  add_column(diversity, richness)
+
+####################################################################
 #Create user interface
 ui <- navbarPage("Amelia's navigation bar",
                  theme = shinytheme("cerulean"),
-                 tabPanel("First tab!",
+                 tabPanel("First tab is DONE!!",
                           h1("first tab header"),
                           p("here's some regular text"),
                           sidebarLayout(
                             sidebarPanel("here's some text",
-                                         radioButtons(inputId="focal",
+                                         selectInput(inputId="pickaphylum",
                                                      label="pick a phylum!",
                                                      choices=unique(reef_tidy$phylum)),
                                          pickerInput(inputId="coocurring",
@@ -72,82 +122,102 @@ ui <- navbarPage("Amelia's navigation bar",
                                                      multiple = TRUE)),
                             mainPanel("some more text is now here",
                                       plotOutput(outputId="plot1")
-                            ))),
-                 tabPanel("Second tab!",
-                          h1("second tab header"),
-                          p("here's some more regular text"),
-                          sidebarLayout(
-                            sidebarPanel("some text is here",
-                                         radioButtons(inputId="locationselect",
-                                                            label="pick a location!",
-                                                            choices=unique(reef_tidy$location))),
-                            mainPanel("some more text is here",
-                                      plotOutput(outputId="plot2"))
-                          )),
-                 tabPanel("Third tab!!",
-                          h1("third tab header"),
-                          p("here's even more regular text"),
-                          sidebarLayout(
-                            sidebarPanel("text be here",
-                                         radioButtons(inputId = "pickacolor", 
-                                                     label = "pick a color!",
-                                                     choices = c("RED!!"="red", "PURPLE!!"="purple", "ORAAAANGE!!!"="orange", "YELLOW!!"="yellow", "GREEEEEN!!"="green")),
-                                         radioButtons(inputId="mapit",
-                                                     label="pick a phylum!",
-                                                     choices=unique(reef_tidy$phylum)),
-                                         radioButtons(inputId="pickavalue",
-                                                      label="pick an output!",
-                                                      choices=c("mean"="mean_count", "sd"="sd_count"))),
-                            mainPanel("some more text is here",
-                                      leafletOutput("mysupercoolmap"))
-                          )))
+                            ))) #,
+                 # tabPanel("Second tab!",
+                 #          h1("second tab header"),
+                 #          p("here's some more regular text"),
+                 #          sidebarLayout(
+                 #            sidebarPanel("some text is here",
+                 #                         radioButtons(inputId="locationselect",
+                 #                                            label="pick a location!",
+                 #                                            choices=unique(reef_tidy$location))),
+                 #            mainPanel("some more text is here",
+                 #                      plotOutput(outputId="plot2"))
+                 #          )),
+                 # tabPanel("Third tab!!",
+                 #          h1("third tab header"),
+                 #          p("here's even more regular text"),
+                 #          sidebarLayout(
+                 #            sidebarPanel("text be here",
+                 #                         radioButtons(inputId = "pickacolor", 
+                 #                                     label = "pick a color!",
+                 #                                     choices = c("RED!!"="red", "PURPLE!!"="purple", "ORAAAANGE!!!"="orange", "YELLOW!!"="yellow", "GREEEEEN!!"="green")),
+                 #                         radioButtons(inputId="mapit",
+                 #                                     label="pick a phylum!",
+                 #                                     choices=unique(reef_tidy$phylum)),
+                 #                         radioButtons(inputId="pickavalue",
+                 #                                      label="pick an output!",
+                 #                                      choices=c("mean"="mean_count", "sd"="sd_count"))),
+                 #            mainPanel("some more text is here",
+                 #                      leafletOutput("mysupercoolmap"))
+                          )
 
 # Create server
 server <- function(input, output){
   
-  reef_select <- reactive({
-    reef_tidy %>%
-      mutate(focal_phylum=input$focal) %>%
-      mutate(presence = ifelse(phylum==focal_phylum, filename, "FALSE")) %>%
-      filter(filename %in% presence) %>%
-      filter(phylum==c(input$coocurring))
-  })
+  # reef_select <- reactive({
+  #   reef_tidy %>%
+  #     mutate(focal_phylum=input$focal) %>%
+  #     mutate(presence = ifelse(phylum==focal_phylum, filename, "FALSE")) %>%
+  #     filter(filename %in% presence) %>%
+  #     filter(phylum==c(input$coocurring))
+  # })
+  # 
+  # output$plot1 <- renderPlot({
+  #   ggplot(reef_select(), aes(x=phylum)) +
+  #     geom_bar(aes(phylum)) +
+  #     coord_flip()
+  # })
+  # 
+  # reef_site <- reactive({
+  #   reef_tidy %>%
+  #     filter(location==input$locationselect)
+  # })
+  # 
+  # output$plot2 <- renderPlot({
+  #   ggplot(reef_site(), aes(x=phylum)) +
+  #     geom_bar(aes(phylum))
+  # })
   
-  output$plot1 <- renderPlot({
-    ggplot(reef_select(), aes(x=phylum)) +
-      geom_bar(aes(phylum)) +
-      coord_flip()
-  })
+  # reef_summary <- reactive({
+  #   reef_tidy %>%
+  #     group_by(location,phylum) %>% #group by location, then lat/long
+  #     summarize(mean_count = mean(value), #get the mean count
+  #               sd_count = sd(value), #get the s.d. count
+  #               sample_size = n()) %>%  #get the sample size
+  #     filter(phylum==c(input$mapit))
+  # })
   
-  reef_site <- reactive({
-    reef_tidy %>%
-      filter(location==input$locationselect)
+## Subset for a phylum
+### This needs to be fixed to possibly facet wrap? because the more phyla you select, the dataframe filters using AND not OR function
+reef_phylum <- reactive({
+  reef_tidy %>%
+    filter(binary > "0") %>% #filter out species not present
+    mutate(focal_phylum=input$pickaphylum) %>% #pick a focal phylum (BASED ON INPUT)
+    mutate(to_match = ifelse(phylum==focal_phylum, filename, "FALSE")) %>% #create a column that we can subset all rows in a plot based on the presence of focal phylum in the plot at least once
+    filter(filename %in% to_match) %>% #if focal phylum is present, keep all observations of that plot ("filename")
+    filter(phylum %in% c(input$coocurring)) %>% #select only the coocurring phyla you want to look at (BASED ON INPUT)
+    distinct(filename, phylum, .keep_all=TRUE) #remove duplicate phylum observations within the same plot
   })
-  
-  output$plot2 <- renderPlot({
-    ggplot(reef_site(), aes(x=phylum)) +
-      geom_bar(aes(phylum))
-  })
-  
-  reef_summary <- reactive({
-    reef_dontfuckthisup %>%
-      group_by(location,phylum) %>% #group by location, then lat/long
-      summarize(mean_count = mean(value), #get the mean count
-                sd_count = sd(value), #get the s.d. count
-                sample_size = n()) %>%  #get the sample size
-      filter(phylum==c(input$mapit))
-  })
-  
-  output$mysupercoolmap <- renderLeaflet({
-    reef_map <- tm_basemap("Esri.WorldImagery") +
-      tm_shape(reef_summary()) +
-      tm_symbols(id="location", col = input$pickacolor, size = input$pickavalue, scale=2) +
-      tm_facets(by = "phylum")
-    
-    tmap_leaflet(reef_map)
-  })
+
+output$plot1 <- renderPlot({
+  ggplot(reef_phylum(), aes(x=fct_rev(phylum))) +
+    geom_bar() +
+    coord_flip()
+   })
+
+
+#   output$mysupercoolmap <- renderLeaflet({
+#     reef_map <- tm_basemap("Esri.WorldImagery") +
+#       tm_shape(reef_summary()) +
+#       tm_symbols(id="location", col = input$pickacolor, size = input$pickavalue, scale=2) +
+#       tm_facets(by = "phylum")
+#     
+#     tmap_leaflet(reef_map)
+#   })
 }
 
 
+####################################################################
 # Let R know you want to combine ui and server into an app
 shinyApp(ui=ui, server=server)
